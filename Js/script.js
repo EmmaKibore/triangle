76e7b721199df5f22a47ed1897189df3ddf8c50d
EmmaKibore <emmakibore@gmail.com>
2019-02-15 16:17:58 +0300

    add initial changes

---
+++ Js/script.js
@@ -0,0 +1,13 @@
+//user interface
+var sideA = parseInt(prompt("Enter first side"));
+var sideB = parseInt(prompt("Enter second side"));
+var sideC = parseInt(prompt("Enter third side"));
+//business logic
+if(sideA == sideB && sideB == sideC && sideC == sideA) {
+alert("Equilateral triangle");
+}
+else if(sideA == sideB || SideB == sideC || sideC == sideA) {
+alert("Isosceles triangle");
+}
+else if("sideA != sideB && sideB != sideC && sideC != sideA")
+alert("Scalene triangle");
