018f410174cbdeb826a0b045975e0b9c9fd78ecb
EmmaKibore <emmakibore@gmail.com>
2019-02-15 16:53:11 +0300

    initial changes

--- Js/script.js
+++ Js/script.js
@@ -3,11 +3,15 @@ var sideA = parseInt(prompt("Enter first side"));
 var sideB = parseInt(prompt("Enter second side"));
 var sideC = parseInt(prompt("Enter third side"));
 //business logic
+if (sideA + sideB <=sideC || sideB + sideC <= sideA || sideC + sideA <= sideB) {
+alert("Cannot form a triangle");
+}
 if(sideA == sideB && sideB == sideC && sideC == sideA) {
 alert("Equilateral triangle");
 }
 else if(sideA == sideB || SideB == sideC || sideC == sideA) {
 alert("Isosceles triangle");
 }
-else if("sideA != sideB && sideB != sideC && sideC != sideA")
+else if(sideA != sideB && sideB != sideC && sideC != sideA) {
 alert("Scalene triangle");
+}
