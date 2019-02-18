0454bf9ce7e173eb3e2d0b58beaa8022ba5481ff
EmmaKibore <emmakibore@gmail.com>
2019-02-18 12:57:07 +0300

    add initial changes

--- Js/script.js
+++ Js/script.js
@@ -1,21 +1,24 @@
 
 
 function checkTriangle() {
-
   var sideA = parseInt(document.getElementById("sidea").value);
   var sideB = parseInt(document.getElementById("sideb").value);
   var sideC = parseInt(document.getElementById("sidec").value);
-  alert("values -- " + sideA);
-  if (sideA + sideB <=sideC || sideB + sideC <= sideA || sideC + sideA <= sideB) {
+  console.log(sideA);
+  console.log(sideB);
+  console.log(sidec);
+  if (sideA + sideB <=sideC && sideB + sideC <= sideA && sideC + sideA <= sideB) {
   alert("Cannot form a triangle");
   }
-  if(sideA == sideB && sideB == sideC && sideC == sideA) {
+  if(sideA === sideB && sideB === sideC && sideC === sideA) {
   alert("Equilateral triangle");
   }
-  else if(sideA == sideB || SideB == sideC || sideC == sideA) {
+  else if(sideA === sideB && sideB!==sideC || sideB === sideC&&sideB!==sideA || sideC === sideA&&sideB!==sideC) {
   alert("Isosceles triangle");
   }
-  else if(sideA != sideB && sideB != sideC && sideC != sideA) {
+  else if(sideA !== sideB && sideB !== sideC && sideC !== sideA && sideA+sideB>sideC&&sideB+sideC>sideA&&sideA+sideC>sideB) {
   alert("Scalene triangle");
-  }
+} else {
+  alert("Cannot form a triangle")
+}
 }
